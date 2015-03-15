f43504b82c3a0c538000f1a29f9758d7238220bc
Inkycrea <inky@inkycrea.net>
2015-03-15 13:31:13 +0100

    Fix cleanDrafts() with custom field
    Corrects the problem of cleanDrafts() procedure if there is a custom field.

--- Model/Behavior/DraftBehavior.php
+++ Model/Behavior/DraftBehavior.php
@@ -1,31 +1,33 @@
 <?php
-class DraftBehavior extends ModelBehavior{
+
+class DraftBehavior extends ModelBehavior {
 
     public $options = array(
         'conditions' => array('online' => -1)
     );
 
-    public function setup(Model $model, $config = array()){
+    public function setup(Model $model, $config = array()) {
         $this->options[$model->alias] = array_merge($this->options, $config);
     }
 
-    public function getDraftId(Model $model, $conditions = array()){
-        $conditions = array_merge( $this->options[$model->alias]['conditions'], $conditions);
+    public function getDraftId(Model $model, $conditions = array()) {
+        $conditions = array_merge($this->options[$model->alias]['conditions'], $conditions);
         $result = $model->find('first', array(
             'fields' => $model->primaryKey,
             'conditions' => $conditions
         ));
-        if(!empty($result)){
+        if (!empty($result)) {
             return $result[$model->alias][$model->primaryKey];
-        }else{
+        } else {
             $model->create($conditions);
             $model->save(null, false);
             return $model->id;
         }
     }
 
-    public function cleanDrafts(Model $model){
-        return $model->deleteAll(array('online' => -1));
+    public function cleanDrafts(Model $model, $conditions = array()) {
+        $conditions = array_merge($this->options[$model->alias]['conditions'], $conditions);
+        return $model->deleteAll($conditions);
     }
 
 }
